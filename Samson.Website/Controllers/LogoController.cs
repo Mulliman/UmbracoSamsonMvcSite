12313c78c197a5580b6c49de23a5a44c57d1fb0b
Sam Mullins <sammullins.co.uk@gmail.com>
2014-01-25 14:26:10 +0000

    Working controllers

---
+++ Samson.Website/Controllers/LogoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Samson.Services;
+using Samson.Standard.Mvc;
+using Samson.Website.Models;
+
+namespace Samson.Website.Controllers
+{
+    public class LogoController : SamsonSurfaceController
+    {
+        public LogoController(IStrongContentService contentService, IStrongMediaService mediaService)
+            : base(contentService, mediaService)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View("~/Views/Partials/LogoView.cshtml", new LogoModel { Url = "http://google.com" });
+        }
+    }
+}
---
+++ Samson.Website/Models/LogoModel.cs
@@ -0,0 +1,11 @@
+﻿using Samson.Standard.MediaTypes;
+
+namespace Samson.Website.Models
+{
+    public class LogoModel
+    {
+        public Image Image { get; set; }
+
+        public string Url { get; set; }
+    }
+}
