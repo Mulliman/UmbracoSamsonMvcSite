dd68e4d087c65086678001e1f7b7b931ea87c7aa
Sam Mullins <sammullins.co.uk@gmail.com>
2014-01-25 13:51:41 +0000

    Add Umbraco to all the things.

---
+++ Samson.Website/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+
+namespace Samson.Website
+{
+    public class Startup : ApplicationEventHandler
+    {
+        //protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        //{
+        //    SamsonContext.Current.DocumentTypesProvider = new AttributeDocumentTypeProvider(Assembly.GetExecutingAssembly());
+        //    //SamsonContext.Current.DocumentTypesProvider.RegisterModelTypes(modelTypes);
+        //    SamsonContext.Current.StrongContentService = new StrongContentService();
+
+        //    SamsonContext.Current.MediaTypesProvider = new AttributeMediaTypeProvider(Assembly.GetExecutingAssembly());
+        //    //SamsonContext.Current.MediaTypesProvider.RegisterModelTypes(mediaModelTypes);
+        //    SamsonContext.Current.StrongMediaService = new StrongMediaService();
+
+        //    ControllerBuilder.Current.SetControllerFactory(
+        //        new Samson.Standard.Mvc.StandardSamsonControllerFactory()
+        //   );
+        //}
+    }
+}
